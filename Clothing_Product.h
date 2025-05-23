886d098202ad32a0a9857ed950b86b6eff32ab52
JesusHiguera8 <itsskype10@gmail.com>
2025-05-22 18:21:18 -0600

    Add files via upload

---
+++ Clothing_Product.h
@@ -0,0 +1,20 @@
+#ifndef CLOTHING_PRODUCT
+#define CLOTHING_PRODUCT 
+
+#include <string>
+using namespace std;
+
+class Clothing_Product:public Product {
+    private: 
+        string fabric;
+        string size;
+    public:
+        Clothing_Product();
+        Clothing_Product(float,int,string,float,float,string,string);
+        string get_fabric();
+        void set_fabric(string); 
+        string get_size();
+        void set_size(string);
+};  
+
+#endif
