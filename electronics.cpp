1cf06d5f9e7603a38b956cb6283fe11e8fd01c06
mausalazar404 <a01712976@tec.mx>
2025-05-22 18:17:14 -0600

    Add files via upload

---
+++ electronics.cpp
@@ -0,0 +1,30 @@
+#include "electronics.h"
+#include <string>
+using namespace std;
+
+
+Electronics::Electronics(){}
+
+Electronics::Electronics(float price,int ID,string name,
+    float discountApplied,float finalPrice,string brand,
+    string warranty):Product(price,ID,name,discountApplied,finalPrice){
+        this->brand = brand;
+        this->warranty = warranty
+    }
+
+
+Electronics::get_brand(){
+    return brand;
+}
+
+Electronics::get_warranty(){
+    return warranty;
+}
+
+Electronics::set_brand(string brand){
+    this->brand = brand;
+}
+
+Electronics::set_warranty(string warranty){
+    this->warranty = warranty;
+}
---
+++ electronics.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Product.h"
+#include <string>
+using namespace std;
+
+
+class Electronics:public Product{
+    private:
+        string brand;
+        string warranty;
+
+    public:
+        Electronics();
+        Electronics(float,int,string,float,float,string,string);
+
+        string get_brand();
+        string get_warranty();
+        void set_brand(string);
+        void set_warranty(string);
+
+};
